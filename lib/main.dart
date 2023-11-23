c0f9627c3271b59875f9d32f6191e2fbd2ba1c9d
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2023-11-23 11:28:42 +0530

    Initialising firebasse

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:campus_connect_app/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding
@@ -21,7 +22,9 @@ void main() async {
       ),
     );
   } else {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
   runApp(const MyApp());
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity, //full width of device
+        child: const Column(
+          children: [],
+        ),
+      )),
+    );
+  }
+}
