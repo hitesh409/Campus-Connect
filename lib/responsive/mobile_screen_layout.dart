1dcbefc3be51d709b1e9a88c6b6f2c57ddfee74e
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2023-11-26 18:54:11 +0530

    Mobile bottom Appbar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:campus_connect_app/providers/user_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:campus_connect_app/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:campus_connect_app/models/user.dart' as model;
@@ -12,54 +13,130 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     model.User user = Provider.of<UserProvider>(context).getUser;
 
     return Scaffold(
-      body: Center(
-        child: Text("Welcome ${user.username} (${user.userid})"),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          Center(
+            child: Text("Home"),
+          ),
+          Center(
+            child: Text("Chat"),
+          ),
+          Center(
+            child: Text("Create"),
+          ),
+          Center(
+            child: Text("Event"),
+          ),
+          Center(
+            child: Text("Notification"),
+          ),
+        ],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: _page == 0
+                ? const Icon(
+                    Icons.home_rounded,
+                    color: primaryColor,
+                  )
+                : const Icon(
+                    Icons.home_rounded,
+                    color: secondaryColor,
+                  ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: _page == 1
+                ? const Icon(
+                    Icons.messenger,
+                    color: primaryColor,
+                  )
+                : const Icon(
+                    Icons.messenger_outline,
+                    color: secondaryColor,
+                  ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: _page == 2
+                ? const Icon(
+                    Icons.add_circle,
+                    color: primaryColor,
+                  )
+                : const Icon(
+                    Icons.add_circle_outline,
+                    color: secondaryColor,
+                  ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: _page == 3
+                ? const Icon(
+                    Icons.event_available,
+                    color: primaryColor,
+                  )
+                : const Icon(
+                    Icons.event_available_outlined,
+                    color: secondaryColor,
+                  ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: _page == 4
+                ? const Icon(
+                    Icons.notifications,
+                    color: primaryColor,
+                  )
+                : const Icon(
+                    Icons.notifications_none,
+                    color: secondaryColor,
+                  ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----  Doesn't seem good as it costs database too much instead we should use State Management using provider package as above which is one of the convenient ways 
-
-// String username = "";
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getUserName();
-//   }
-
-//   void getUserName() async {
-//     //get the current user details from user collection
-//     DocumentSnapshot snapshot = await FirebaseFirestore.instance
-//         .collection('users')
-//         .doc(FirebaseAuth.instance.currentUser!.uid)
-//         .get();
-
-//     setState(() {
-//       username = (snapshot.data() as Map<String, dynamic>)['username'];
-//     });
-//   }
